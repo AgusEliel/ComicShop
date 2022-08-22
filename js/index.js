6ecfc0ed780c6f4a5afe3a54b9e76bab3771e08c
Agus Eliel <aguseliel2@gmail.com>
2022-08-22 04:59:48 -0300

    fix cart & shop

--- js/index.js
+++ js/index.js
@@ -1,56 +1,83 @@
-let stockComics = [
-    { id: 1, nombre: "Comic Batman", cantidad: 1, precio: 1200, img: "./comics/batman.jpg" },
-    { id: 2, nombre: "Comic Flash", cantidad: 1, precio: 1200, img: "./comics/flash.jpg" },
-    { id: 3, nombre: "Comic Spiderman", cantidad: 1, precio: 1200, img: "./comics/spiderman.jpg" },
-    { id: 4, nombre: "Comic Superman", cantidad: 1, precio: 1200, img: "./comics/superman.jpg" },
-    { id: 5, nombre: "Comic The Boys", cantidad: 1, precio: 1200, img: "./comics/theboys.jpg" },
-    { id: 6, nombre: "Comic Gambito", cantidad: 1, precio: 1200, img: "./comics/gambito.jpg" },
-    { id: 7, nombre: "Comic NightWing", cantidad: 1, precio: 1200, img: "./comics/nightwing.jpg" },
-    { id: 8, nombre: "Comic Avengers", cantidad: 1, precio: 1200, img: "./comics/avengers.jpg" },
-];
-
 const contenedorComics = document.getElementById("shop__content");
 
-const carritoComics = document.getElementById("comics__carrito");
+const carritoComics = document.getElementById("carrito__contenedor");
+
+const botonVaciar = document.getElementById("vaciar__carrito");
+
+const contadorCarrito = document.getElementById("contador__carrito");
+
+const precioTotal = document.getElementById("precioTotal");
 
 let carrito = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("carrito")) {
+        carrito = JSON.parse(localStorage.getItem("carrito"));
+        actualizarCarrito();
+    }
+});
+
+botonVaciar.addEventListener("click", () => {
+    carrito.length = 0;
+    actualizarCarrito();
+});
+
 stockComics.forEach((comic) => {
     const div = document.createElement("div");
-    div.classList.add("comic");
+    div.classList.add("revistas");
     div.innerHTML = `
     <img src=${comic.img} alt="">
     <p class="shop__contenttitle__title">${comic.nombre}</p>
-    <p class="shop__contenttitle__precio">Precio: $${comic.precio}</p>
-    <button type='button' id="agregar${comic.id}" class="shop__content__art__btn">AGREGAR AL CARRITO</button>
+    <p class="shop__contenttitle__precio" >Precio: $${comic.precio}</p>
+    <button id="agregar${comic.id}" class="shop__content__art__btn">AGREGAR AL CARRITO</button>
+    
     `;
-
     contenedorComics.appendChild(div);
 
-    const button = document.getElementById("agregar${comic.id}");
+    const boton = document.getElementById(`agregar${comic.id}`);
 
-    button?.addEventListener("click", () => {
+    boton.addEventListener("click", () => {
         agregarAlCarrito(comic.id);
     });
 });
 
 const agregarAlCarrito = (prodId) => {
-    const item = stockComics.find((prod) => prod.id === prodId);
-    carrito.push(item);
+    const existe = carrito.some((prod) => prod.id === prodId);
+    if (existe) {
+        const prod = carrito.map((prod) => {
+            if (prod.id === prodId) {
+                prod.cantidad++;
+            }
+        });
+    } else {
+        const item = stockComics.find((prod) => prod.id === prodId);
+        carrito.push(item);
+    }
     actualizarCarrito();
-    console.log(carrito);
+};
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId);
+    const indice = carrito.indexOf(item);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
 };
 
 const actualizarCarrito = () => {
-    contenedorComics.innerHTML = "";
+    carritoComics.innerHTML = "";
     carrito.forEach((prod) => {
         const div = document.createElement("div");
         div.className = "comicEnCarrito";
         div.innerHTML = `
         <p>${prod.nombre}</p>
         <p>Precio: $${prod.precio}</p>
-        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-        <button onclick = "eliminarDelCarrito(${prod.id})" class="boton__eliminar fas fa-trash-alt"></button>
+        <p>Cantidad: <span id="cantidad"> ${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton__eliminar fas fa-trash-alt"></button>
         `;
-        contenedorComics.appendChild(div);
+        carritoComics.appendChild(div);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
     });
+    contadorCarrito.innerText = carrito.length;
+
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0);
 };
