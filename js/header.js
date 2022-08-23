4d4178f2b82f0a7b5630dbc6d87dfd6fd2ea79fb
Agus Eliel <98791637+AgusEliel@users.noreply.github.com>
2022-08-23 09:30:29 -0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,30 +2,14 @@
     let doc = document.documentElement;
     let w = window;
 
-    /*
-    define four variables: curScroll, prevScroll, curDirection, prevDirection
-    */
+   
 
     let curScroll;
     let prevScroll = w.scrollY || doc.scrollTop;
     let curDirection = 0;
     let prevDirection = 0;
 
-    /*
-    how it works:
-    -------------
-    create a scroll event listener
-    create function to check scroll position on each scroll event,
-    compare curScroll and prevScroll values to find the scroll direction
-    scroll up - 1, scroll down - 2, initial - 0
-    then set the direction value to curDirection
-    compare curDirection and prevDirection
-    if it is different, call a function to show or hide the header
-    example:
-    step 1: user scrolls down: curDirection 2, prevDirection 0 > hide header
-    step 2: user scrolls down again: curDirection 2, prevDirection 2 > already hidden, do nothing
-    step 3: user scrolls up: curDirection 1, prevDirection 2 > show header
-    */
+   
 
     let header = document.getElementById("header");
     let toggled;
